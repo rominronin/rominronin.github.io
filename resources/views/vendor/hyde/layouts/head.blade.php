c5370ade364ca0a66b18f9d6adc2534110d4dd56
Baris Tosun <baris@acolono.com>
2024-02-16 22:14:30 +0100

    publish layout templates, add google analytics

---
+++ resources/views/vendor/hyde/layouts/head.blade.php
@@ -0,0 +1,28 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9BTWGF9E5G"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-9BTWGF9E5G');
+</script>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>{{ $page->title() }}</title>
+
+@if (file_exists(Hyde::mediaPath('favicon.ico')))
+    <link rel="shortcut icon" href="{{ Hyde::relativeLink('media/favicon.ico') }}" type="image/x-icon">
+@endif
+
+{{-- App Meta Tags --}}
+@include('hyde::layouts.meta')
+
+{{-- App Stylesheets --}}
+@include('hyde::layouts.styles')
+
+@if(Hyde::hasFeature('darkmode'))
+    {{-- Check the local storage for theme preference to avoid FOUC --}}
+    <meta id="meta-color-scheme" name="color-scheme" content="{{ config('hyde.default_color_scheme', 'light') }}">
+    <script>if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) { document.documentElement.classList.add('dark'); document.getElementById('meta-color-scheme').setAttribute('content', 'dark');} else { document.documentElement.classList.remove('dark') } </script>
+@endif
